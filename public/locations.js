ae32a1d78dd15d240714020bff5d31960fbe6a59
Kaspy2 <gfarrug@gmail.com>
2021-04-13 22:29:19 +0200

    Updated styles, added spinning logo and maps links

--- public/locations.js
+++ public/locations.js
@@ -1,17 +1,64 @@
 
+var locations = [];
+
+function stringCheck(s) {
+    return (typeof s === 'string' || s instanceof String);
+}
+
 function makeLoc(loc) {
-    return `<li>${loc.location}, ${loc.hint} at ${loc.coordinates[0]},${loc.coordinates[1]}</li>`
+    let locName = `<h4>${loc.location}</h4>`;
+    let locHints = "";
+
+    if (stringCheck(loc.hint)) {
+        locHints = `<p>${loc.hint}</p>`;
+    }
+    else {
+        // list of hints
+        locHints = loc.hint.map(x => `<p>${x}</p>`).join("")
+    }
+
+    let locLink = `<p><a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${loc.coordinates[0]},${loc.coordinates[1]}">Maps</a></p>`;
+
+    return `<div class="locationCard">${locName}${locHints}${locLink}</div>`
 }
 
 function setLocs(locs) {
-    const locsContainer = document.getElementById("locsContainer");
+    let locsContainer = document.getElementById("locsContainer");
 
-    for (var i = 0; i < locs.length; i++) {
-        locsContainer.innerHTML += makeLoc(locs[i]);
+    locsContainer.innerHTML = "";
+    if (locations.length == 0) { // if startup
+        locations = locs;
+
+        // replicate to increase
+        for (var x = 0; x < 3; x++) {
+            locations = [...locations, ...locations];
+        }
+
+        for (var i = 0; i < locations.length; i++) {
+            locsContainer.innerHTML += makeLoc(locations[i]);
+        }
     }
+    else {
+        for (var i = 0; i < locs.length; i++) {
+            locsContainer.innerHTML += makeLoc(locs[i]);
+        }
+    }
+}
+
+function filterLocs(e) {
+    let filterText = e.target.value;
+
+    function checkLoc(l) {
+        return (l.location.toLowerCase().includes(filterText) || (stringCheck(l.hint) ? l.hint : l.hint.join("")).toLowerCase().includes(filterText))
+    }
+
+    let filteredLocs = locations.filter(checkLoc);
+    setLocs(filteredLocs);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     fetch("locs.json").then(response => response.json()).then(setLocs);
+
+    document.getElementById("filterText").addEventListener('input', filterLocs)
 });
 
